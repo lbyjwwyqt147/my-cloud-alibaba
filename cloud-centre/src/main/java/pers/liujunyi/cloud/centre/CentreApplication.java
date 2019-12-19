e9050029ea6de9cc9a1c93d8d1f1bc36436dab0b
lbyjwwyqt147 <ljy671018>
2019-12-19 15:24:31 +0800

    初始化

--- cloud-centre/src/main/java/pers/liujunyi/cloud/centre/CentreApplication.java
+++ cloud-centre/src/main/java/pers/liujunyi/cloud/centre/CentreApplication.java
@@ -6,10 +6,12 @@ import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.context.request.RequestContextListener;
 import pers.liujunyi.cloud.common.configuration.MySQLUpperCaseStrategy;
 import pers.liujunyi.cloud.common.encrypt.annotation.EnableEncrypt;
 
@@ -26,6 +28,16 @@ import pers.liujunyi.cloud.common.encrypt.annotation.EnableEncrypt;
 @ComponentScan(basePackages = {"pers.liujunyi.cloud"}, excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {MySQLUpperCaseStrategy.class}))
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class,  RedisAutoConfiguration.class, RedisRepositoriesAutoConfiguration.class})
 public class CentreApplication {
+
+    /**
+     * 监听器：监听HTTP请求事件
+     * 解决RequestContextHolder.getRequestAttributes()空指针问题
+     * @return
+     */
+    @Bean
+    public RequestContextListener requestContextListener(){
+        return new RequestContextListener();
+    }
 
     public static void main(String[] args) {
         // 解决 Factory method 'elasticsearchClient' threw exception; nested exception is java.lang.IllegalStateException: availableProcessors is already set to [4], rejecting [4]
