cf7cf11ddb7b128dc0a9962982c04dbdb5b7393b
lbyjwwyqt147 <ljy671018>
2020-01-03 15:52:41 +0800

    初始化

--- cloud-gateway/src/main/java/pers/liujunyi/cloud/gateway/CloudGatewayApplication.java
+++ cloud-gateway/src/main/java/pers/liujunyi/cloud/gateway/CloudGatewayApplication.java
@@ -3,9 +3,11 @@ package pers.liujunyi.cloud.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@ComponentScan(basePackages = {"pers.liujunyi.cloud"})
 public class CloudGatewayApplication {
 
     public static void main(String[] args) {
--- cloud-gateway/src/main/java/pers/liujunyi/cloud/gateway/configuration/WebSecurityConfig.java
+++ cloud-gateway/src/main/java/pers/liujunyi/cloud/gateway/configuration/WebSecurityConfig.java
@@ -1,27 +1,34 @@
 package pers.liujunyi.cloud.gateway.configuration;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 /**
+ *
  * @author ljy
  */
-@Configuration
-@EnableWebSecurity
-@Order(99)
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//@Configuration
+@EnableWebFluxSecurity
+//@EnableOAuth2Sso
+public class WebSecurityConfig  {
 
     /**
      * 禁止csrf
      * @param http
      * @throws Exception
      */
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    protected void configure(ServerHttpSecurity http) throws Exception {
         http.csrf().disable();
+    }
+
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+
+        http.csrf().disable();
+
+        return http.build();
     }
 
 }
